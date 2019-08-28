d640db8265d4264196fde015ba1f797c66982df6
gerardo <gerardo@cenac84.cenac.ipn.mx>
2019-08-27 19:17:41 -0500

    Terminando pantalla registrar atributo y modificaciones en el JS

---
+++ Desarrollo/src/main/java/mx/tesseract/editor/bs/TipoDatoBs.java
@@ -0,0 +1,29 @@
+package mx.tesseract.editor.bs;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import mx.tesseract.dao.GenericoDAO;
+import mx.tesseract.editor.entidad.TipoDato;
+import mx.tesseract.util.TESSERACTException;
+
+@Service("tipoDatoBs")
+@Scope(value = BeanDefinition.SCOPE_SINGLETON)
+public class TipoDatoBs {
+	
+	@Autowired
+	private GenericoDAO genericoDAO;
+	
+	public List<TipoDato> consultarTiposDato() {
+		List<TipoDato> listTiposDato = genericoDAO.findAll(TipoDato.class);
+		if (listTiposDato.isEmpty()) {
+			throw new TESSERACTException("No se pueden consultar los tipos de dato.", "MSG12");
+		}
+		return listTiposDato;
+	}
+
+}
